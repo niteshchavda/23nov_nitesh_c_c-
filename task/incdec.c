d77b491235018c3157bd11587e895b53a04ab3eb
niteshchavda <119494971+niteshchavda@users.noreply.github.com>
2022-12-02 16:00:18 +0530

    Add files via upload

---
+++ task/incdec.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("enter the value 2022 a:");
+	scanf("%d",&a);
+	printf("value of no is:%d",a);
+	a++;
+	printf("\nincrinment the value is 2023:%d",a);
+	a--;
+	a--;
+	printf("\ndecriment the value is 2021:%d ",a);
+}
