e2316e823ca44c4ab77b7e0d3a4fcb5e367ff1df
niteshchavda <119494971+niteshchavda@users.noreply.github.com>
2022-12-02 15:47:25 +0530

    Add files via upload

---
+++ assignment/Module-1/addresh.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+	printf("my addresh is= at-bhadthar ta-jam khambhaliya dis-devbhumi dwarka");
+}
---
+++ assignment/Module-1/borthdate.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+	printf("my birth date is:06/06/2001");
+}
