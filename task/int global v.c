cf68a93ff42d8a04625a506e0cf40f00a3150b6a
niteshchavda <119494971+niteshchavda@users.noreply.github.com>
2022-12-02 15:59:03 +0530

    Add files via upload

---
+++ task/int global v.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int no=500; //globle value
+void main()
+{
+	
+	printf("value of no is:%d",no);
+}
---
+++ task/int static v.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main()
+{
+	int no=670; //static value
+	printf("value of no is:%d",no);
+}
