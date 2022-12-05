29f89837b6d1edf9943bcb56528187fd29613e3f
niteshchavda <119494971+niteshchavda@users.noreply.github.com>
2022-12-05 15:28:33 +0530

    Add files via upload

---
+++ task/iselse.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	printf("enter the value of 2 no");
+	scanf("%d%d",&a,&b);
+	//a=15,b=18
+	if(a<=b)
+	{
+		printf("eligible for voat");
+	}
+	else
+	{
+		printf("not eligible for voat");
+	}
+}
---
+++ task/nagativepo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("enter the value of  no");
+	scanf("%d",&a);
+	if(a<=0)
+	{
+		printf("value is positive ");
+	}
+	else
+	{
+		printf("value is nagative");
+	}
+}
